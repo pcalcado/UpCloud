a52538973d04f5295a7b444c27b0e934158e2847
Phil Calçado <pcalcado@gmail.com>
2011-07-17 11:44:41 +0100

    skeleton

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein run -m upcloud.web
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject upcloud "1.0.0-SNAPSHOT"
+  :description "awesome music deserves the cloud"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [ring/ring-jetty-adapter "0.3.9"]])
